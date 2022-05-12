02123d9973cfcaec9373318b072ee10aa4dbd5c9
= <=>
2022-05-12 20:22:18 +0900

    add blog.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Meyondong`,
+    title: `meyongdong`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
     "gatsby-plugin-styled-components", {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
