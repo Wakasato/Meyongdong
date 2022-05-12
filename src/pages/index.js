b95b2d75b9afb91e255ae00e99feb438d7fcdcc1
= <=>
2022-05-12 21:05:52 +0900

    "Finished Gatsby Tutorial Part 6"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
